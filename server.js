e12e9ceda4eb7a0dff3d8a9449682a7f0cb426cb
Asanka Madushan <85320753+NnAsankaMadushan@users.noreply.github.com>
2023-06-03 09:49:25 +0530

    connecting mongodb

--- server.js
+++ server.js
@@ -4,3 +4,26 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const app = express(); //variable
+
+const port = process.env.PORT || 8070;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL,{
+    useCreateIndex:true,
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useFindAndModify:false
+});
+
+const connection = mongoose.connection;
+connection.once("open",()=>{
+    console.log("Connected to MongoDB");
+})
+
+app.listen(PORT,()=>{
+    console.log("Server Started on port ${PORT}");
+})
